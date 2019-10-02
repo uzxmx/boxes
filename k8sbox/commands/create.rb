9c482610b2807fd972569c2cc41b46113e2745b9
Mingxiang Xue <mingxiangxue@gmail.com>
2019-10-03 00:17:39 +0800

    Add option for taint master

--- k8sbox/commands/create.rb
+++ k8sbox/commands/create.rb
@@ -3,8 +3,9 @@ class Create < Commands::Create
     config.num_instances = prompt.ask('Number of instances:', default: 2, convert: :int)
     config.instance_name_prefix = prompt.ask('Instance name prefix:', default: 'k8s')
     config.subnet = prompt.ask('Cluster subnet (e.g. 172.17.9):', value: '172.17.', required: true)
+    config.taint_master = prompt.yes?('Taint master (pods will not be scheduled to master)?', default: true)
 
     config.ansible_playbooks_path = 'ansible_playbooks'
-    dry_run or system("sh -c 'cd #{@project_name} && git submodule add https://github.com/uzxmx/ansible_playbooks.git'")
+    dry_run or system("sh -c 'cd #{@project_name} && git submodule add https://github.com/uzxmx/ansible_playbooks.git && cd ansible_playbooks && git submodule update --init --recursive'")
   end
 end
